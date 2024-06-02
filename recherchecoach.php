ebb43c40c7b5b380249b46c4bf2ff0340b351e87
Alexia <alexia.farigoul@edu.ece.fr>
2024-06-02 19:41:50 +0200

    bug

--- recherchecoach.php
+++ recherchecoach.php
@@ -3,51 +3,50 @@
     $database = "sportify";
     $db_handle = mysqli_connect('localhost', 'root', '');
     $db_found = mysqli_select_db($db_handle, $database);
-    $choice = isset($_POST["button2"]) ? $_POST["button2"] : "";
-
+    $choice = isset($_POST["button1"]) ? $_POST["button1"] : "";
+    
     $recherche = isset($_POST["recherche"]) ? $_POST["recherche"] : "";
     if ($db_found) {
         $erreur = false;
         $errorMessage = "";
-
-
+        
         switch ($choice) {
             case "":
-                $sql = "SELECT * FROM clients WHERE prenomclient LIKE '%" . $recherche . "%' OR nomclient LIKE '%" . $recherche . "%'";
+                $sql = "SELECT * FROM coaches WHERE prenom LIKE '%" . $recherche . "%' OR nom LIKE '%" . $recherche . "%' OR specialite LIKE '%" . $recherche . "%'";
 
                 break;
-
+            
             default:
-                $sql = "SELECT * FROM clients WHERE prenomclient = 'Paul'";
+                $sql = "SELECT * FROM coaches WHERE prenom = 'Alice'";
                 break;
         }
-
+        
         if (!$erreur) {
             $result = mysqli_query($db_handle, $sql);
-            echo "<p>Requete : ". $sql ."</p>";
             echo "<table border=\"1\">";
             echo "<tr>";
-            echo "<th>ID</th>";
             echo "<th>Nom</th>";
             echo "<th>Prénom</th>";
-            echo "<th>Email</th>";
-            echo "<th>Adresse</th>";
+            echo "<th>Spécialité</th>";
+            echo "<th>Salle</th>";
+            echo "<th>Photo</th>";
             echo "</tr>";
 
             // Afficher le résultat
             while ($data = mysqli_fetch_assoc($result)) {
                 echo "<tr>";
-                echo "<td>" . $data['idclient'] . "</td>";
-                echo "<td>" . $data['nomclient'] . "</td>";
-                echo "<td>" . $data['prenomclient'] . "</td>";
-                echo "<td>" . $data['emailclient'] . "</td>";
-                echo "<td>" . $data['adresseclient'] . "</td>";
+                echo "<td>" . $data['prenom'] . "</td>";
+                echo "<td>" . $data['nom'] . "</td>";
+                echo "<td>" . $data['specialite'] . "</td>";
+                echo "<td>" . $data['num_salle'] . "</td>";
+                $image = $data['photo'];
+                echo "<td><img src='$image' height='80' width='80'></td>";
                 echo "</tr>";
             }
             echo "</table>";
         } else {
             echo $errorMessage . "<br>";
-        }
+        } 
     } else {
         echo "<br>Database not found";
     }
